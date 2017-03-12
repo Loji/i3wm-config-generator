bc54def7aa22e9e57cff8f11417e473ff3b19709
Loji <jakub.lechocki@gmail.com>
2017-03-12 21:45:39 +0100

    Adding normalize.css, font-awesome

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 'use strict';
 
 require('./index.html'); // copy index into dist
+require('normalize.css/normalize.css');
+require('./style.scss'); 
+require('font-awesome/css/font-awesome.css');
+
 
 var Elm = require('./Main.elm');
 var mountNode = document.getElementById('main');
