ef15fa58c2bc1edc720d17e36f171c4293bbac33
Loji <jakub.lechocki@gmail.com>
2017-03-25 20:04:46 +0100

    Partially fixing slight movement of tiles when adding new one

--- src/Tile/View.elm
+++ src/Tile/View.elm
@@ -10,54 +10,62 @@ import Tile.Model as Tile
 
 view : Tile.Model -> Html Msg
 view model =
-    let
-        tileLayout =
-            if model.layout == Tile.Horizontal then
-                "tileContainter--horizontal"
-            else
-                "tileContainter--vertical"
+    if model.visible then
+        let
+            tileLayout =
+                if model.layout == Tile.Horizontal then
+                    "tileContainter--horizontal"
+                else
+                    "tileContainter--vertical"
 
-        toggleLayoutIcon =
-            if model.layout == Tile.Horizontal then
-                "fa-arrows-v"
-            else
-                "fa-arrows-h"
+            toggleLayoutIcon =
+                if model.layout == Tile.Horizontal then
+                    "fa-arrows-v"
+                else
+                    "fa-arrows-h"
 
-        isTile =
-            List.length (Tile.getChildTiles model.tiles) == 0
+            isTile =
+                List.length (Tile.getChildTiles model.tiles) == 0
 
-        tileType =
-            if isTile then
-                "tile"
-            else
-                "tileContainter"
-
-        tileContent =
-            if isTile then
-                [ div [ class "tileTitle" ] [ text "Title" ] ]
-            else
-                (List.map view (Tile.getChildTiles model.tiles))
-    in
-        div
-            [ class
-                (tileType ++ " " ++ tileLayout)
-            ]
-            (List.append
-                [ div [ class "tileActionButtons" ]
-                    [ if isTile then
-                        text ""
-                      else
-                        i
-                            [ onClick (Msg.ModifyTile Tile.ChangeLayout model.id)
-                            , class ("fa " ++ toggleLayoutIcon)
+            tileContent =
+                if isTile then
+                    [ div [ class "tileTitle" ] [ text "Title" ] ]
+                else
+                    [ div
+                        [ class "tileChildren" ]
+                        [ div
+                            [ class ("tileContainer " ++ tileLayout) ]
+                            (List.map view (Tile.getChildTiles model.tiles))
+                        ]
+                    ]
+        in
+            div
+                [ class
+                    ("tile " ++ tileLayout)
+                ]
+                (List.append
+                    [ div [ class "tileActionButtons" ]
+                        [ if isTile then
+                            text ""
+                          else
+                            i
+                                [ onClick (Msg.ModifyTile Tile.ChangeLayout model.id)
+                                , class ("fa " ++ toggleLayoutIcon)
+                                ]
+                                []
+                        , i
+                            [ onClick (Msg.AddTile model.id)
+                            , class ("fa fa-plus")
                             ]
                             []
-                    , i
-                        [ onClick (Msg.AddTile model.id)
-                        , class ("fa fa-plus")
+                        , i
+                            [ onClick (Msg.ModifyTile Tile.HideTile model.id)
+                            , class ("fa fa-trash-o")
+                            ]
+                            []
                         ]
-                        []
                     ]
-                ]
-                tileContent
-            )
+                    tileContent
+                )
+    else
+        text ""
